6e45a4d88e4eb6fd7372c6cdcafb34d7e48b020f
Mackenzie Revoyr <makr28@gmail.com>
2020-04-26 13:08:23 -0400

    start

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace ItunesToSpotifyForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.convertBtn = new System.Windows.Forms.Button();
+            this.messageLbl = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.Location = new System.Drawing.Point(713, 415);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.Size = new System.Drawing.Size(75, 23);
+            this.convertBtn.TabIndex = 0;
+            this.convertBtn.Text = "Convert";
+            this.convertBtn.UseVisualStyleBackColor = true;
+            this.convertBtn.Click += new System.EventHandler(this.convertBtn_Click);
+            // 
+            // messageLbl
+            // 
+            this.messageLbl.AutoSize = true;
+            this.messageLbl.Location = new System.Drawing.Point(12, 428);
+            this.messageLbl.Name = "messageLbl";
+            this.messageLbl.Size = new System.Drawing.Size(0, 13);
+            this.messageLbl.TabIndex = 1;
+            this.messageLbl.Click += new System.EventHandler(this.messageLbl_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.messageLbl);
+            this.Controls.Add(this.convertBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button convertBtn;
+        private System.Windows.Forms.Label messageLbl;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using SpotifyAPI.Web.Enums;
+using SpotifyAPI.Web.Models;
+
+namespace ItunesToSpotifyForm
+{
+    public partial class Form1 : Form
+    {
+        SpotifyWebAPI SpotifyAPI = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object senderForm, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"C:\dev\spotifyConfig\config.xml");
+            String clientId = doc.SelectNodes("//spotifyConfig/clientId")[0].InnerText;
+            String clientSecret = doc.SelectNodes("//spotifyConfig/clientSecret")[0].InnerText;
+
+            AuthorizationCodeAuth auth = new AuthorizationCodeAuth(
+              clientId,
+              clientSecret,
+              "http://localhost:4002",
+              "http://localhost:4002",
+              Scope.PlaylistReadPrivate | Scope.PlaylistReadCollaborative
+            );
+
+            auth.AuthReceived += async (sender, payload) =>
+            {
+                auth.Stop();
+                Token token = await auth.ExchangeCode(payload.Code);
+                SpotifyAPI = new SpotifyWebAPI()
+                {
+                    TokenType = token.TokenType,
+                    AccessToken = token.AccessToken
+                };
+
+            };
+            auth.Start(); // Starts an internal HTTP Server
+            auth.OpenBrowser();
+
+        }
+
+        private void convertBtn_Click(object sender, EventArgs e)
+        {
+            if (SpotifyAPI != null)
+            {
+                createSpotifyPlaylists();
+            } else
+            {
+                this.messageLbl.Text = "Not Ready... Not Ready.. Stop That!";
+            }
+        }
+
+        private void createSpotifyPlaylists()
+        {
+            this.messageLbl.Text = "Ready!";
+        }
+
+        private void messageLbl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
